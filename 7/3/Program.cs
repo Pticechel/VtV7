e941d7d35d6d357d235d66b66f4b074ae9ff3f3f
Pticechel <70794998+Pticechel@users.noreply.github.com>
2023-03-11 16:31:31 +0300

    Add files via upload

---
+++ 7/3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncDelegateExample
+{
+    // Определяем делегат, принимающий два параметра int и возвращающий int
+    delegate Task<int> MatrixDelegate(int rows, int cols);
+
+    class Program
+    {
+        static async Task<int> MatrixOperation(int rows, int cols, IProgress<int> progress)
+        {
+            // Создаем матрицу целых случайных чисел
+            int[,] matrix = new int[rows, cols];
+            Random rnd = new Random();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = rnd.Next(1, 100);
+                }
+            }
+
+            // Вычисляем максимальное и минимальное значения
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] > max)
+                    {
+                        max = matrix[i, j];
+                    }
+                    if (matrix[i, j] < min)
+                    {
+                        min = matrix[i, j];
+                    }
+                }
+
+                // Обновляем информацию о ходе выполнения операции
+                if (progress != null)
+                {
+                    int percentComplete = (i + 1) * 100 / rows;
+                    progress.Report(percentComplete);
+                }
+
+                // Делаем паузу в 100 миллисекунд, чтобы не перегружать процессор
+                await Task.Delay(100);
+            }
+
+            // Возвращаем разницу между максимальным и минимальным значениями
+            return max - min;
+        }
+
+        static async Task Main(string[] args)
+        {
+            // Создаем экземпляр делегата и передаем в него метод MatrixOperation
+            MatrixDelegate matrixDelegate = new MatrixDelegate(MatrixOperation);
+
+            // Создаем объект прогресса, чтобы выводить информацию о ходе выполнения операции
+            Progress<int> progress = new Progress<int>();
+            progress.ProgressChanged += (sender, percentComplete) =>
+            {
+                Console.CursorLeft = 0;
+                Console.Write($"Calculating... {percentComplete}%");
+            };
+
+            // Вызываем метод асинхронно и ждем его завершения
+            Task<int> task = matrixDelegate.Invoke(5, 5, progress);
+
+            // Ожидаем завершения операции с тайм-аутом в 10 секунд
+            if (await Task.WhenAny(task, Task.Delay(10000)) != task)
+            {
+                Console.WriteLine("\nOperation timed out!");
+            }
+            else
+            {
+                Console.WriteLine($"\nResult: {await task}");
+            }
+        }
+    }
+}
---
+++ 7/Program.cs
@@ -0,0 +1,66 @@
+﻿public class StringPair
+{
+    public string Original { get; }
+    public string Encrypted { get; }
+
+    public StringPair(string original)
+    {
+        Original = original;
+        Encrypted = EncryptString(original);
+    }
+
+    private string EncryptString(string original)
+    {
+        char[] chars = original.ToCharArray();
+        for (int i = 0; i < chars.Length; i++)
+        {
+            chars[i] = (char)(chars[i] + 1);
+        }
+        return new string(chars);
+    }
+}
+
+public class StringPairCollection
+{
+    private List<StringPair> _pairs = new List<StringPair>();
+
+    public void Add(string original)
+    {
+        _pairs.Add(new StringPair(original));
+    }
+
+    public void ProcessPairsWithThreadPool()
+    {
+        int threadsCount = Environment.ProcessorCount;
+        ThreadPool.SetMaxThreads(threadsCount, threadsCount);
+
+        foreach (var pair in _pairs)
+        {
+            ThreadPool.QueueUserWorkItem(ProcessPair, pair);
+        }
+
+        Thread.Sleep(1000);
+    }
+
+    private void ProcessPair(object obj)
+    {
+        StringPair pair = (StringPair)obj;
+        Console.WriteLine($"Original string: {pair.Original}, Encrypted string: {pair.Encrypted}, Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(100);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringPairCollection collection = new StringPairCollection();
+        collection.Add("Hello");
+        collection.Add("World");
+        collection.Add("C# is awesome!");
+
+        collection.ProcessPairsWithThreadPool();
+
+        Console.ReadKey();
+    }
+}
